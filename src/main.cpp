b559361c86753333d6f87dabfc5c55d67eb0a9e8
Liam Teale <liamteale6@gmail.com>
2025-02-01 16:45:30 -0800

    improve test code

--- src/main.cpp
+++ src/main.cpp
@@ -11,4 +11,10 @@ void initialize() {
   pros::lcd::initialize();
   pros::lcd::print(0, "initializing: %d", copro::init(13, 921600));
   pros::lcd::print(1, "errno: %d", errno);
+  pros::delay(10);
+  while (true) {
+    auto a = otos::get_pose();
+    std::cout << "x: " << a.x << ", y: " << a.y << ", h: " << a.h << std::endl;
+    pros::delay(10);
+  }
 }
