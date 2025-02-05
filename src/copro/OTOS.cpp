a04784a1b3c05e3f06305d9055d2ef6e252ba002
Liam Teale <liamteale6@gmail.com>
2025-02-04 23:22:40 -0800

    implement OTOS::get_status

--- src/copro/OTOS.cpp
+++ src/copro/OTOS.cpp
@@ -19,7 +19,39 @@ using Version = OTOS::Version;
 // macros
 /////////////////
 
+// macro to simplify returning an ERROR
 #define ERROR(...) std::unexpected<_Error>(_Error(__VA_ARGS__))
+
+// macro to simplify checking write_and_receive
+#define CHECK_WRITE(response, num)                                                                                     \
+    if (!response) {                                                                                                   \
+        return std::unexpected(_Error(response.error(), RS485_IO,                                                      \
+                                      "failed to send command to OTOS with id {} on port {}", m_device,                \
+                                      m_coprocessor->get_port()));                                                     \
+    }                                                                                                                  \
+    if (response->size() != num) {                                                                                     \
+        return std::unexpected<_Error>(_Error(INCORRECT_RESPONSE_SIZE,                                                 \
+                                              "expected response size of {} from OTOS with id {} on port {}", num,     \
+                                              m_device, m_coprocessor->get_port()));                                   \
+    }
+
+// macro to simplify checking whether the OTOS has been initialized
+#define CHECK_INITIALIZE()                                                                                             \
+    if (!m_initialized) {                                                                                              \
+        return std::unexpected<_Error>(_Error(NOT_INITIALIZED, "OTOS with id {} on port {} has not been initialized",  \
+                                              m_device, m_coprocessor->get_port()));                                   \
+    }
+
+// macro to simplify returning an I2C_IO error
+#define I2CERR                                                                                                         \
+    std::unexpected<_Error>(                                                                                           \
+        _Error(I2C_IO, "failed to interact with OTOS with id {} on port {}", m_device, m_coprocessor->get_port()));
+
+// macro to simplify returning an unknown error
+#define UNKNOWNERR                                                                                                     \
+    std::unexpected<_Error>(_Error(UNKNOWN,                                                                            \
+                                   "unknown error response when trying to interact with OTOS with id {} on port {}",   \
+                                   m_device, m_coprocessor->get_port()));
 
 //////////////////////////////////////
 // constants
@@ -34,7 +66,8 @@ namespace topic {
 constexpr const char* INITIALIZE = "otos/initialize";
 constexpr const char* GET_HARDWARE_VERSION = "otos/get_hardware_version";
 constexpr const char* GET_FIRMWARE_VERSION = "otos/get_firmware_version";
-constexpr const char* SELF_TEST = "otos/self_test";
+constexpr const char* SELF_TEST_WRITE = "otos/self_test_write";
+constexpr const char* SELF_TEST_READ = "otos/self_test_read";
 constexpr const char* CALIBRATE_IMU = "otos/calibrate_imu";
 constexpr const char* IS_IMU_CALIBRATED = "otos/is_imu_calibrated";
 constexpr const char* GET_LINEAR_SCALAR = "otos/get_linear_scalar";
@@ -101,79 +134,109 @@ std::expected<void, _Error> OTOS::initialize() {
     for (auto c : m_device) out.push_back(c);
     // send data
     auto raw = m_coprocessor->write_and_receive(topic::INITIALIZE, out, READ_TIMEOUT);
-    // check for errors
-    if (!raw) {
-        return std::unexpected(_Error(raw.error(), RS485_IO, "failed to send command to OTOS with id {} on port {}",
-                                      m_device, m_coprocessor->get_port()));
+    // check for IO errors
+    CHECK_WRITE(raw, 1);
+    // check respose
+    switch (raw->at(0)) {
+        case 0: {
+            m_initialized = true;
+            return {};
+        }
+        case 1: {
+            m_initialized = true;
+            // TODO: add warning here
+            return {};
+        }
+        case 2: return I2CERR;
+        default: return UNKNOWNERR;
     }
-    if (raw->size() != 1) {
-        return ERROR(INCORRECT_RESPONSE_SIZE, "incorrect response size from OTOS with id {} on port {}", m_device,
-                     m_coprocessor->get_port());
-    }
-    if (raw->at(0) == 1) {
-        // the OTOS was initialized before this program was ran, probably
-        // not a big deal, so we eat the "error"
-        m_initialized = true;
-        return {};
-    }
-    if (raw->at(0) == 2) {
-        return ERROR(I2C_IO, "failed to interact with OTOS with id {} on port {}", m_device, m_coprocessor->get_port());
-    }
-    if (raw->at(0) != 0) {
-        return ERROR(UNKNOWN, "unknown error response when trying to interact with OTOS with id {} on port {}",
-                     m_device, m_coprocessor->get_port());
-    }
-    // no errors
-    return {};
 }
 
 std::expected<Version, _Error> OTOS::get_hardware_version() {
     // check that the OTOS has been initialized
-    if (m_initialized) {
-        return ERROR(ALREADY_INITIALIZED, "OTOS with id {} on port {} has already been initialized", m_device,
-                     m_coprocessor->get_port());
-    }
+    CHECK_INITIALIZE();
     // send data
     auto raw = m_coprocessor->write_and_receive(topic::GET_HARDWARE_VERSION, {}, READ_TIMEOUT);
-    // check for errors
-    if (!raw) {
-        return std::unexpected(_Error(raw.error(), RS485_IO, "failed to send command to OTOS with id {} on port {}",
-                                      m_device, m_coprocessor->get_port()));
+    // check for IO errors
+    CHECK_WRITE(raw, 2);
+    // check respose
+    switch (raw->at(0)) {
+        case std::numeric_limits<uint8_t>::max(): return I2CERR;
+        default: return Version(raw->at(0), raw->at(1));
     }
-    if (raw->size() != 2) {
-        return ERROR(INCORRECT_RESPONSE_SIZE, "incorrect response size from OTOS with id {} on port {}", m_device,
-                     m_coprocessor->get_port());
-    }
-    if (raw->at(0) == std::numeric_limits<uint8_t>::max()) {
-        return ERROR(I2C_IO, "failed to interact with OTOS with id {} on port {}", m_device, m_coprocessor->get_port());
-    }
-    return Version {.major = raw->at(0), .minor = raw->at(1)};
 }
 
 std::expected<Version, _Error> OTOS::get_firmware_version() {
     // check that the OTOS has been initialized
-    if (m_initialized) {
-        return ERROR(ALREADY_INITIALIZED, "OTOS with id {} on port {} has already been initialized", m_device,
-                     m_coprocessor->get_port());
-    }
+    CHECK_INITIALIZE();
     // send data
     auto raw = m_coprocessor->write_and_receive(topic::GET_FIRMWARE_VERSION, {}, READ_TIMEOUT);
-    // check for errors
-    if (!raw) {
-        return std::unexpected(_Error(raw.error(), RS485_IO, "failed to send command to OTOS with id {} on port {}",
-                                      m_device, m_coprocessor->get_port()));
+    // check for IO errors
+    CHECK_WRITE(raw, 2);
+    // check respose
+    switch (raw->at(0)) {
+        case std::numeric_limits<uint8_t>::max(): return I2CERR;
+        default: return Version(raw->at(0), raw->at(1));
     }
-    if (raw->size() != 2) {
-        return ERROR(INCORRECT_RESPONSE_SIZE, "incorrect response size from OTOS with id {} on port {}", m_device,
-                     m_coprocessor->get_port());
+}
+
+std::expected<bool, Error<ErrorType>> OTOS::self_test() {
+    union {
+            struct {
+                    /// @brief Write 1 to start the self test
+                    uint8_t start : 1;
+
+                    /// @brief Returns 1 while the self test is in progress
+                    uint8_t inProgress : 1;
+
+                    /// @brief Returns 1 if the self test passed
+                    uint8_t pass : 1;
+
+                    /// @brief Returns 1 if the self test failed
+                    uint8_t fail : 1;
+
+                    /// @brief Reserved bits, do not use
+                    uint8_t reserved : 4;
+            };
+
+            /// @brief Raw register value
+            uint8_t value;
+    } res;
+
+    // check that the OTOS has been initialized
+    CHECK_INITIALIZE();
+    // send self test register
+    auto raw = m_coprocessor->write_and_receive(topic::SELF_TEST_WRITE, {}, READ_TIMEOUT);
+    // check for IO errors
+    CHECK_WRITE(raw, 2);
+    // check respose
+    switch (raw->at(0)) {
+        case 0: break; // no errors
+        case 1: return I2CERR;
+        default: return UNKNOWNERR;
     }
-    if (raw->at(0) == std::numeric_limits<uint8_t>::max()) {
-        return ERROR(I2C_IO, "failed to interact with OTOS with id {} on port {}", m_device, m_coprocessor->get_port());
+
+    // loop until self test is done
+    for (int i = 0; i < 10; i++) {
+        pros::delay(5); // short delay between reads
+        res.start = 1;
+        auto raw = m_coprocessor->write_and_receive(topic::SELF_TEST_READ, {res.start}, READ_TIMEOUT);
+        // check IO errors
+        CHECK_WRITE(raw, 1);
+        res.value = raw->at(0);
+        // check response
+        if (res.value == std::numeric_limits<uint8_t>::max()) return I2CERR;
+        if (!res.inProgress) break;
     }
-    return Version {.major = raw->at(0), .minor = raw->at(1)};
+
+    // check if the self test passed
+    return static_cast<bool>(res.pass == 1);
 }
 
 std::expected<Status, _Error> OTOS::get_status() {
+    // check that the OTOS has been initialized
+    CHECK_INITIALIZE();
+
     // the info we receive uses bit fields
     union {
             struct {
@@ -189,16 +252,10 @@ std::expected<Status, _Error> OTOS::get_status() {
 
     // get raw data from OTOS, and check for errors
     auto raw = m_coprocessor->write_and_receive(topic::GET_STATUS, {}, READ_TIMEOUT);
-    if (!raw) {
-        return std::unexpected(_Error(raw.error(), RS485_IO, "failed to send command to OTOS with id {} on port {}",
-                                      m_device, m_coprocessor->get_port()));
-    }
-    if (raw->size() != 2) return ERROR(INCORRECT_RESPONSE_SIZE, "response from coprocessor too short");
+    CHECK_WRITE(raw, 1);
     // parse raw data
     s.value = raw->at(0);
-    if (s.value == std::numeric_limits<uint8_t>::max()) {
-        return ERROR(I2C_IO, "failed to interact with OTOS with id {} on port {}", m_device, m_coprocessor->get_port());
-    }
+    if (s.value == std::numeric_limits<uint8_t>::max()) return I2CERR;
     // return data
     return Status {
         .warn_tilt_angle = static_cast<bool>(s.warn_tilt_angle),
