182f8fcf412bae960039d1885dd3649d6cd40bc8
Liam Teale <liamteale6@gmail.com>
2025-02-04 20:45:41 -0800

    simplify Error class

--- include/copro/Error.hpp
+++ include/copro/Error.hpp
@@ -1,26 +1,10 @@
 #pragma once
 
 #include <format>
-#include <sstream>
-#include <string>
-#include <source_location>
-#include <type_traits>
 #include <vector>
+#include <ostream>
 
 namespace copro {
-
-/**
- * @brief Get the name of enum values
- *
- * This function needs to be specialized to work with a given type
- *
- * @tparam T the enum
- * @param err the value
- * @return std::string the name of the enum value as a string
- */
-template <typename T>
-    requires std::is_scoped_enum_v<T>
-std::string get_error_name(T err);
 
 /**
  * @brief Generic Error class
@@ -49,24 +33,13 @@ class Error {
             what.push_back(std::format(fmt, std::forward<Args>(args)...));
         }
 
-        /**
-         * @brief get the error as a string
-         *
-         * @return std::string
-         */
-        std::string as_string(std::source_location loc = std::source_location::current()) {
-            std::ostringstream out;
-            out << get_error_name<T>(type) << " Error at:" << std::endl;
-            out << "  " << loc.file_name() << '(' << loc.line() << ':' << loc.column() << ")" << std::endl;
-            out << "in function:" << std::endl;
-            out << "  " << loc.function_name() << std::endl;
-            out << "with message:" << std::endl;
-            for (auto s : what) out << "  " << s << std::endl;
-            return out.str();
-        }
-
         const T type;
         std::vector<std::string> what;
 };
+
+template <typename T> std::ostream& operator<<(std::ostream& os, const Error<T>& e) {
+    for (auto it = e.what.rbegin(); it != e.what.rend(); it++) os << "  " << *it << std::endl;
+    return os;
+}
 
 } // namespace copro
