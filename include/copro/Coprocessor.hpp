8fd42fc47f3dac2c7df8c4944c798d61896b3b20
Liam Teale <liamteale6@gmail.com>
2025-02-12 22:35:03 -0800

    improve style

--- include/copro/Coprocessor.hpp
+++ include/copro/Coprocessor.hpp
@@ -8,53 +8,48 @@
 
 namespace copro {
 
-// protocol:
-// it's all COBS'd
-// [8-bit ID] [CRC16] [payload] [0x0]
-
 /**
  * @brief Coprocessor class. Simplifies interactions with a coprocessor over a
  * smart port
  */
 class Coprocessor {
-    public:
-        enum class ErrorType {
-            READ_TIMEOUT,
-            INVALID_PORT,
-            PORT_UNAVAILABLE,
-            NO_MESSAGE,
-            MESSAGE_CORRUPTED,
-            MESSAGE_TOO_BIG,
-            IO_FAILURE,
-            NOT_INITIALIZED,
-            ALREADY_INITIALIZED,
-            UNKNOWN_FAILURE,
-            UNKNOWN_FAILURE_COPROCESSOR,
-            TOO_MANY_TOPICS,
-        };
+  public:
+    enum class ErrorType {
+        READ_TIMEOUT,
+        INVALID_PORT,
+        PORT_UNAVAILABLE,
+        NO_MESSAGE,
+        MESSAGE_CORRUPTED,
+        MESSAGE_TOO_BIG,
+        IO_FAILURE,
+        NOT_INITIALIZED,
+        ALREADY_INITIALIZED,
+        UNKNOWN_FAILURE,
+        UNKNOWN_FAILURE_COPROCESSOR,
+        TOO_MANY_TOPICS,
+    };
 
-    public:
-        Coprocessor(int port, int baud_rate);
-        /**
-         * @brief
-         *
-         * @return std::unexpected<Error<ErrorType>>
-         */
-        std::expected<int, Error<ErrorType>> initialize();
-        std::expected<std::vector<uint8_t>, Error<ErrorType>>
-        write_and_receive(const std::string& topic,
-                          const std::vector<uint8_t>& data,
-                          int timeout);
-        int get_port();
+  public:
+    Coprocessor(int port, int baud_rate);
+    /**
+     * @brief
+     *
+     * @return std::unexpected<Error<ErrorType>>
+     */
+    std::expected<int, Error<ErrorType>> initialize();
+    std::expected<std::vector<uint8_t>, Error<ErrorType>>
+    write_and_receive(const std::string& topic,
+                      const std::vector<uint8_t>& data,
+                      int timeout);
+    int get_port();
 
-    private:
-        std::expected<uint8_t, Error<ErrorType>>
-        find_id(const std::string& topic);
+  private:
+    std::expected<uint8_t, Error<ErrorType>> find_id(const std::string& topic);
 
-        const int m_port;
-        const int m_baud_rate;
-        bool m_initialized = false;
-        pros::Mutex m_mutex;
-        std::vector<std::string> m_topics = { "register", "ping", "version" };
+    const int m_port;
+    const int m_baud_rate;
+    bool m_initialized = false;
+    pros::Mutex m_mutex;
+    std::vector<std::string> m_topics = { "register", "ping", "version" };
 };
 } // namespace copro
--- include/copro/Error.hpp
+++ include/copro/Error.hpp
@@ -10,131 +10,130 @@ namespace copro {
 template<typename T>
     requires std::is_scoped_enum_v<T>
 class Error {
-    public:
-        /**
-         * @brief create a new error
-         *
-         * @tparam Args std::format arguments types
-         *
-         * @param type the type of error
-         * @param fmt formatter string
-         * @param args formatter arguments
-         *
-         * @return constexpr std::unexpected<Error>
-         */
-        template<typename... Args>
-        static constexpr std::unexpected<Error<T>>
-        make(T type, std::format_string<Args...> fmt, Args&&... args) noexcept {
-            return std::unexpected<Error<T>>(
-              Error<T>(type,
-                       { std::format(fmt, std::forward<Args>(args)...) }));
-        }
+  public:
+    /**
+     * @brief create a new error
+     *
+     * @tparam Args std::format arguments types
+     *
+     * @param type the type of error
+     * @param fmt formatter string
+     * @param args formatter arguments
+     *
+     * @return constexpr std::unexpected<Error>
+     */
+    template<typename... Args>
+    static constexpr std::unexpected<Error<T>>
+    make(T type, std::format_string<Args...> fmt, Args&&... args) noexcept {
+        return std::unexpected<Error<T>>(
+          Error<T>(type, { std::format(fmt, std::forward<Args>(args)...) }));
+    }
 
-        /**
-         * @brief add more info to an error
-         *
-         * @tparam R the expected value
-         * @tparam Args std::format arguments types
-         *
-         * @param other the error to add to
-         * @param fmt formatter string
-         * @param args formatter arguments
-         *
-         * @return constexpr std::unexpected<Error<T>>
-         */
-        template<typename R, typename... Args>
-        static constexpr std::unexpected<Error<T>>
-        add(const std::expected<R, Error<T>>& other,
-            std::format_string<Args...> fmt,
-            Args&&... args) {
-            auto tmp = other.error();
-            tmp.what.push_back(std::format(fmt, std::forward<Args>(args)...));
-            return std::unexpected<Error<T>>(tmp);
-        }
+    /**
+     * @brief add more info to an error
+     *
+     * @tparam R the expected value
+     * @tparam Args std::format arguments types
+     *
+     * @param other the error to add to
+     * @param fmt formatter string
+     * @param args formatter arguments
+     *
+     * @return constexpr std::unexpected<Error<T>>
+     */
+    template<typename R, typename... Args>
+    static constexpr std::unexpected<Error<T>>
+    add(const std::expected<R, Error<T>>& other,
+        std::format_string<Args...> fmt,
+        Args&&... args) {
+        auto tmp = other.error();
+        tmp.what.push_back(std::format(fmt, std::forward<Args>(args)...));
+        return std::unexpected<Error<T>>(tmp);
+    }
 
-        /**
-         * @brief add more info to an error
-         *
-         * @tparam R the expected value
-         * @tparam Args std::format arguments types
-         *
-         * @param other the error to add to
-         * @param fmt formatter string
-         * @param args formatter arguments
-         *
-         * @return constexpr std::unexpected<Error<T>>
-         */
-        template<typename R, typename... Args>
-        static constexpr std::unexpected<Error<T>>
-        add(std::expected<R, Error<T>>&& other,
-            std::format_string<Args...> fmt,
-            Args&&... args) {
-            other.what.push_back(std::format(fmt, std::forward<Args>(args)...));
-            return std::unexpected<Error<T>>(std::move(other));
-        }
+    /**
+     * @brief add more info to an error
+     *
+     * @tparam R the expected value
+     * @tparam Args std::format arguments types
+     *
+     * @param other the error to add to
+     * @param fmt formatter string
+     * @param args formatter arguments
+     *
+     * @return constexpr std::unexpected<Error<T>>
+     */
+    template<typename R, typename... Args>
+    static constexpr std::unexpected<Error<T>>
+    add(std::expected<R, Error<T>>&& other,
+        std::format_string<Args...> fmt,
+        Args&&... args) {
+        other.what.push_back(std::format(fmt, std::forward<Args>(args)...));
+        return std::unexpected<Error<T>>(std::move(other));
+    }
 
-        /**
-         * @brief add more info to an error
-         *
-         * @tparam R the expected value
-         * @tparam S the type of the error to add to
-         * @tparam Args std::format arguments types
-         *
-         * @param other the error to add to
-         * @param fmt formatter string
-         * @param args formatter arguments
-         *
-         * @return constexpr std::unexpected<Error<T>>
-         */
-        template<typename R, typename S, typename... Args>
-            requires(!std::is_same_v<T, R>)
-        static constexpr std::unexpected<Error<T>>
-        add(T type,
-            const std::expected<R, Error<S>>& other,
-            std::format_string<Args...> fmt,
-            Args&&... args) {
-            std::vector<std::string> tmp = other.error().what;
-            tmp.push_back(std::format(fmt, std::forward<Args>(args)...));
-            return std::unexpected<Error<T>>(Error<T>(type, std::move(tmp)));
-        }
+    /**
+     * @brief add more info to an error
+     *
+     * @tparam R the expected value
+     * @tparam S the type of the error to add to
+     * @tparam Args std::format arguments types
+     *
+     * @param other the error to add to
+     * @param fmt formatter string
+     * @param args formatter arguments
+     *
+     * @return constexpr std::unexpected<Error<T>>
+     */
+    template<typename R, typename S, typename... Args>
+        requires(!std::is_same_v<T, R>)
+    static constexpr std::unexpected<Error<T>>
+    add(T type,
+        const std::expected<R, Error<S>>& other,
+        std::format_string<Args...> fmt,
+        Args&&... args) {
+        std::vector<std::string> tmp = other.error().what;
+        tmp.push_back(std::format(fmt, std::forward<Args>(args)...));
+        return std::unexpected<Error<T>>(Error<T>(type, std::move(tmp)));
+    }
 
-        /**
-         * @brief add more info to an error
-         *
-         * @tparam R the expected value
-         * @tparam S the type of the error to add to
-         * @tparam Args std::format arguments types
-         *
-         * @param other the error to add to
-         * @param fmt formatter string
-         * @param args formatter arguments
-         *
-         * @return constexpr std::unexpected<Error<T>>
-         */
-        template<typename R, typename S, typename... Args>
-            requires(!std::is_same_v<T, R>)
-        static constexpr std::unexpected<Error<T>>
-        add(T type,
-            std::expected<R, Error<S>>&& other,
-            std::format_string<Args...> fmt,
-            Args&&... args) {
-            other.what.push_back(std::format(fmt, std::forward<Args>(args)...));
-            return std::unexpected<Error<T>>(Error<T>(type, std::move(other)));
-        }
+    /**
+     * @brief add more info to an error
+     *
+     * @tparam R the expected value
+     * @tparam S the type of the error to add to
+     * @tparam Args std::format arguments types
+     *
+     * @param other the error to add to
+     * @param fmt formatter string
+     * @param args formatter arguments
+     *
+     * @return constexpr std::unexpected<Error<T>>
+     */
+    template<typename R, typename S, typename... Args>
+        requires(!std::is_same_v<T, R>)
+    static constexpr std::unexpected<Error<T>>
+    add(T type,
+        std::expected<R, Error<S>>&& other,
+        std::format_string<Args...> fmt,
+        Args&&... args) {
+        other.what.push_back(std::format(fmt, std::forward<Args>(args)...));
+        return std::unexpected<Error<T>>(Error<T>(type, std::move(other)));
+    }
 
-        /**
-         * @brief the type of error
-         */
-        const T type;
-        /**
-         * @brief information of the errors
-         */
-        std::vector<std::string> what;
+    /**
+     * @brief the type of error
+     */
+    const T type;
+    /**
+     * @brief information of the errors
+     */
+    std::vector<std::string> what;
 
-    private:
-        Error(T type, std::vector<std::string>&& s) noexcept
-            : type(type),
-              what({ s }) {}
+  private:
+    Error(T type, std::vector<std::string>&& s) noexcept
+        : type(type),
+          what({ s }) {}
 };
 
 template<typename T>
