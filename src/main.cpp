b20b996b43ce681f34dfdf90d346b8fd5484c995
Liam Teale <liamteale6@gmail.com>
2025-12-17 09:27:06 -0800

    add some tests to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
 #include "main.h"
+#include "copro/Coprocessor.hpp"
+#include "copro/OTOS.hpp"
 
-void initialize() {}
+void initialize() {
+    pros::delay(3000);
+    std::cout << "Starting!" << std::endl;
+    copro::init(21, 921600);
+    std::cout << "success!" << std::endl;
+
+    while (true) {
+        auto accel = otos::get_acceleration();
+        std::cout << "accel2: " << accel.x << std::endl;
+        pros::delay(1000);
+    }
+}
